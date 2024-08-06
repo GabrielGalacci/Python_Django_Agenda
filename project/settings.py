9a6ada311fccfc397618a0d1b6abb34c17abc961
Gabriel <gabrielcssoad@hotmail.com>
2024-08-05 21:20:10 -0300

    Changing Postgre to SQLITE

--- project/settings.py
+++ project/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-try:
-    from project.local_settings import *
-except ImportError:
-    ...
+# try:
+#     from project.local_settings import *
+# except ImportError:
+#     ...
